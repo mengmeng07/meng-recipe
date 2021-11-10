a513c29379d21faee69183590abe93b23047241e
Mengzhu Liu <mengzhu.liu07@gmail.com>
2021-11-10 14:23:09 +0100

    Nico's attempt to make it work during lunch

---
+++ text.js
@@ -0,0 +1,16 @@
+
+done[object Object]
+{"type":"doc","content":
+    [
+        {"type":"paragraph","content":
+            [
+                {"type":"text","text":"Hey, this is initial title"}
+            ]
+        }
+    ]
+}
+{"type":"doc","content":
+    [{"type":"paragraph","content":
+        [{"type":"text","text":"Hey, this is initial content"}]
+    }]
+}
